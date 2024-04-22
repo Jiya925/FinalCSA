8cbf8cdab9454e0cd0c3e96386b5ca0df9c12d81
whosamy <114783824+whosamy@users.noreply.github.com>
2024-04-22 11:39:38 -0700

    printSolution() and isValid()

--- Sudoku/src/SolverLogic.java
+++ Sudoku/src/SolverLogic.java
@@ -1,6 +1,36 @@
 
 public class SolverLogic {
-	public SolverLogic() {
+	private int[][] board;
+	
+	public SolverLogic(int[][] initialBoard) {
+        board = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = initialBoard[i][j];
+            }
+        }
+    }
+	
+	public int[][] solve(){
 		
 	}
+	
+	public void printSolution() {
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				System.out.print(board[r][c]);
+			}
+			System.out.println();
+		}
+	}
+	
+	private boolean isValid(int row, int col, int num) {
+        //check if num is already present in the row/column/subgrid
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
