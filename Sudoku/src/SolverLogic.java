3b269b25c0f88a3d5912b9d552eb13f5cbc1949b
whosamy <114783824+whosamy@users.noreply.github.com>
2024-05-02 10:37:33 -0700

    updated easyBoard & SolverLogic

--- Sudoku/src/SolverLogic.java
+++ Sudoku/src/SolverLogic.java
@@ -2,8 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SolverLogic {
-    private int[][] board;
-
+    int[][] board;
+    public int N = 9;
+    
+    //turn hashmap into a 9x9
     public SolverLogic(HashMap<Integer, int[][]> fileBoard) {
         //initialize a new 9x9 board
         board = new int[9][9];
@@ -14,8 +16,9 @@ public class SolverLogic {
             int[][] threeByThree = entry.getValue();
             
             //calculate starting row and column in the 9x9 grid based on the key
-            int rowStart = (key / 3) * 3;
-            int colStart = (key % 3) * 3;
+            int rowStart = ((key-1) / 3) * 3;
+            int colStart = ((key-1) % 3) * 3;
+
             
             //place the 3x3 array into the 9x9 grid at the calculated starting position
             for (int i = 0; i < 3; i++) {
@@ -26,54 +29,103 @@ public class SolverLogic {
                 }
             }
         }
-        solveSudoku(board, 0, 0);
+        System.out.println("Initial Board:");
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        //solve the puzzle
+        solveSudoku();
+        
+        // Print the initial board
+        printSolution();
+
+        // Solve the puzzle
+        boolean solved = solveSudoku();
+
+        // Print the solved board if the puzzle was solved
+        if (solved) {
+            printSolution();
+        } else {
+            System.out.println("Sudoku puzzle could not be solved.");
+        }
     }
 
     //backtracking algorithm
-    public int[][] solveSudoku(int grid[][], int row, int col) {
-        //check if board has been solved completely
-    	if (row == 8 && col == 9)
-            return grid;
-    	
-    	//go to next row
-        if (col == 9) {
+    public boolean solveSudoku() {
+        return solveSudoku(board, 0, 0);
+    }
+    
+    public boolean solveSudoku(int board[][], int row, int col) {
+        // Check if we have reached the end of the board
+        if (row == N - 1 && col == N) {
+            return true;
+        }
+
+        //if we have reached the end of a row, move to the next row
+        if (col == N) {
             row++;
             col = 0;
         }
 
-        //check if there's already a number in that position
-        if (grid[row][col] != 0)
-            return solveSudoku(grid, row, col + 1);
-        
-        //test each num starting from 1 until it works
-        for (int num = 1; num <= 9; num++) {
-            if (isValid(row, col, num)) {
-                grid[row][col] = num;
-                if (solveSudoku(grid, row, col + 1) != null)
-                    return grid;
-            }
-            grid[row][col] = 0; //backtrack and try the next num
+        //if the cell is already filled, move to the next cell
+        if (board[row][col] != 0) {
+            return solveSudoku(board, row, col + 1);
         }
-        return null;
+
+        //try each number from 1 to 9
+        for (int num = 1; num < 10; num++) {
+            if (isValid(board, row, col, num)) {
+                //if the number is valid, place it in the cell
+                board[row][col] = num;
+                //recursively solve for the next cell
+                if (solveSudoku(board, row, col + 1)) {
+                    return true;
+                }
+                //if the solution fails, backtrack and try the next number
+                board[row][col] = 0;
+            }
+        }
+        //if no number works, backtrack
+        return false;
     }
     
     //print filled board
     public void printSolution() {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
                 System.out.print(board[r][c]);
             }
             System.out.println();
         }
     }
     
-    private boolean isValid(int row, int col, int num) {
+    public boolean isValid(int[][] board, int row, int col, int num) {
         //check if num is already present in the row/column/subgrid
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num || board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
+    	for (int x = 0; x <= 8; x++) {
+            if (board[row][x] == num) {
+                return false;
+            }
+    	}
+ 
+        for (int x = 0; x <= 8; x++) {
+            if (board[x][col] == num) {
                 return false;
             }
         }
+ 
+        int startRow = row - row % 3, startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+    		}
+    	}
+ 
         return true;
-    }
+	}
 }
