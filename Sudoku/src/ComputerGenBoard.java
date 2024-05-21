490b9c703332b7faa62eca26a0435ee9c16110e3
beach <beach@10.9.131.232>
2024-05-21 11:45:58 -0700

    Commented and cleaned up CompGenBoard better

--- Sudoku/src/ComputerGenBoard.java
+++ Sudoku/src/ComputerGenBoard.java
@@ -1,14 +1,14 @@
 
 public class ComputerGenBoard {
-	 int[][] board;
-	    int N; // number of cols & rows.
-	    int SRNum; // square root of N
-	    int spots; // number of missing spots --> based on difficulty
-	    // Constructor
+	 	private int[][] board;
+	    private int N;      //number of cols & rows.
+	    private int root;   //square root of N
+	    private int spots;  //number of missing spots --> based on difficulty
+	    
 	    public ComputerGenBoard() {
 	        N = 9;
-	        // find square root of N
-	        SRNum = (int)(Math.sqrt(N));
+	        //square root of N
+	        root = (int)(Math.sqrt(N));
 	        board = new int[N][N];
 	    }
 	   
@@ -16,24 +16,27 @@ public class ComputerGenBoard {
 	    public int[][] getBoard() {
 	    	return board;
 	    }
-	    // fill all the values of the board
+	    
+	    // fills all the values of the board - mother method and others are helpers
 	    public void fillValues() {
-	        // Fill the diagonal of 3 x 3 (SRNum) board
+	        // fills the diagonal of 3x3 (root) board
 	        fillDiagonal();
-	        // Fill remaining blocks
-	        fillRemaining(0, SRNum);
+	        // fills remaining blocks
+	        fillRemaining(0, root);
 	    }
-	    // Fill the 3 diagonal of 3 x 3 boards
-	    void fillDiagonal() {
-	        for (int i = 0; i < N; i = i+SRNum) {
+	    
+	    //fills the 3 diagonal of 3x3 boards
+	    public void fillDiagonal() {
+	        for (int i = 0; i < N; i = i+root) {
 	        	// for diagonal box, start coordinates --> i==j
 	            fillBox(i, i);
 	        }
 	    }
-	    // Returns false if given 3 x 3 block contains num.
+	    
+	    //returns false if given 3x3 block contains num.
 	    boolean unUsedInBox(int rowStart, int colStart, int num) {
-	        for (int i = 0; i<SRNum; i++) {
-	            for (int j = 0; j<SRNum; j++) {
+	        for (int i = 0; i<root; i++) {
+	            for (int j = 0; j<root; j++) {
 	                if (board[rowStart+i][colStart+j]==num) {
 	                    return false;
 	                }
@@ -41,12 +44,15 @@ public class ComputerGenBoard {
 	        }
 	        return true;
 	    }
-	    // Fill a 3 x 3 spot.
-	    void fillBox(int row, int col) {
+	    
+	    //fills a 3x3 spot
+	    //overall a helper method to fill in a 3x3 box
+	    public void fillBox(int row, int col) {
 	        int num;
-	        for (int i = 0; i < SRNum; i++) {
-	            for (int j = 0; j < SRNum; j++) {
+	        for (int i = 0; i < root; i++) {
+	            for (int j = 0; j < root; j++) {
 	            	num = randomGenerator(N);
+	            	//keeps creating as long until random is not in the box already 
 	                while (!unUsedInBox(row, col, num)) {
 	                	num = randomGenerator(N);
 	                }
@@ -54,18 +60,20 @@ public class ComputerGenBoard {
 	            }
 	        }
 	    }
-	    // Random generator
-	    int randomGenerator(int num) {
+	    
+	    //random generator of number 
+	    public int randomGenerator(int num) {
+	    	//floor returns largest possible of the random 
 	        return (int) Math.floor((Math.random()*num+1));
 	    }
-	    // Check if safe to put in cell
-	    boolean CheckIfSafe(int i, int j, int num) {
-	        return (unUsedInRow(i, num) &&
-	                unUsedInCol(j, num) &&
-	                unUsedInBox(i-i%SRNum, j-j%SRNum, num));
+	    
+	    //check if safe to put in cell
+	    public boolean CheckIfSafe(int i, int j, int num) {
+	        return (unUsedInRow(i, num) && unUsedInCol(j, num) && unUsedInBox(i-i%root, j-j%root, num));
 	    }
-	    // check in the row for existence
-	    boolean unUsedInRow(int i, int num) {
+	    
+	    //check in the row for existence of num (i is tracker)
+	    public boolean unUsedInRow(int i, int num) {
 	        for (int j = 0; j < N; j++) {
 	           if (board[i][j] == num) {
 	                return false;
@@ -73,8 +81,9 @@ public class ComputerGenBoard {
 	        }
 	        return true;
 	    }
-	    // check in the row for existence
-	    boolean unUsedInCol(int j, int num) {
+	    
+	    //check in the col for existence of num (j is tracker)
+	    public boolean unUsedInCol(int j, int num) {
 	        for (int i = 0; i < N; i++) {
 	            if (board[i][j] == num) {
 	                return false;
@@ -82,8 +91,9 @@ public class ComputerGenBoard {
 	        }
 	        return true;
 	    }
-	    // A recursive function to fill remaining board
-	    boolean fillRemaining(int i, int j) {
+	    
+	    //recursive function that fills the remaining board
+	    public boolean fillRemaining(int i, int j) {
 	        //  System.out.println(i+" "+j);
 	        if (j >= N && i < N-1) {
 	            i = i + 1;
@@ -92,18 +102,18 @@ public class ComputerGenBoard {
 	        if (i >= N && j >= N) {
 	            return true;
 	        }
-	        if (i < SRNum) {
-	            if (j < SRNum) {
-	                j = SRNum;
+	        if (i < root) {
+	            if (j < root) {
+	                j = root;
 	            }
 	        }
-	        else if (i < N-SRNum) {
-	            if (j == (int)(i / SRNum) * SRNum) {
-	                j =  j + SRNum;
+	        else if (i < N-root) {
+	            if (j == (int)(i / root) * root) {
+	                j =  j + root;
 	            }
 	        }
 	        else {
-	            if (j == N-SRNum) {
+	            if (j == N-root) {
 	                i = i + 1;
 	                j = 0;
 	                if (i >= N) {
@@ -122,35 +132,36 @@ public class ComputerGenBoard {
 	        }
 	        return false;
 	    }
-	    // Remove the spots no. of digits to
-	    // complete game
+	    
+	    //remove the spots number of digits to complete game
 	    public void removespotsDigits(int k) {
 	    	
 	    	if (k == 1) { //easy board
-	    		spots = (int)((Math.random()*10) + 25);
+	    		spots = (int)((Math.random()*10) + 22); //removes 22 to 32 spots
 	    	}
 	    	else if (k == 2) { //medium board
-	    		spots = (int)((Math.random()*10) + 35);
+	    		spots = (int)((Math.random()*11) + 34); //removes 34 to 45 spots
 	    	}
 	    	else if (k == 3) {  //hard board
-	    		spots = (int)((Math.random()*10) + 45);
+	    		spots = (int)((Math.random()*12) + 45); //removes 45 to 57 spots
 	    	}
 	    	
 	        int count = spots;
 	        while (count != 0) {
 	            int cellId = randomGenerator(N*N)-1;
 	            // System.out.println(cellId);
-	            // extract coordinates i  and j
+	            //get coordinates i and j
 	            int i = (cellId / N);
 	            int j = cellId % N;
 	            // System.out.println(i+" "+j);
 	            if (board[i][j] != 0) {
 	                count--;
-	                board[i][j] = 0;
+	                board[i][j] = 0; //sets empty spots to 0
 	            }
 	        }
 	    }
-	    // Print sudoku
+	    
+	    // Print sudoku board
 	    public void printSudoku() {
 	        for (int i = 0; i < N; i++) {
 	            for (int j = 0; j < N; j++) {
